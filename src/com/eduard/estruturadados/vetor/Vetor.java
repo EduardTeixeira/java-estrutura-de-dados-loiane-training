472028c97d45b81898c6b27e7499ecd8b8d752cb
Eduard Teixeira <eduardteixeira01@gmail.com>
2023-12-04 09:33:05 -0300

    Aula 02: Vetores e Arrays - Introdução

---
+++ src/com/eduard/estruturadados/vetor/Vetor.java
@@ -0,0 +1,11 @@
+package com.eduard.estruturadados.vetor;
+
+public class Vetor {
+
+	private String[] elementos;
+
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+
+}
---
+++ src/com/eduard/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,17 @@
+package com.eduard.estruturadados.vetor.teste;
+
+import com.eduard.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(10);
+
+		int i = 0;
+		i++;
+		System.out.println(i);
+
+	}
+
+}
